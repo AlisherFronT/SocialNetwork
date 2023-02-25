b56b2c128b09728dce12d151339a7f46feae8dd3
Alisher <alisher.rysspaevjava@gmail.com>
2023-02-25 19:52:11 +0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import {Suspense} from "react";
+import '../src/styles/style.scss'
+import './utils/i18n'
+import {useSelector} from "react-redux";
+import PrivateRouting from "./routing/PrivateRouting";
+import AuthRouting from "./routing/AuthRouting";
+
+function App() {
+
+  const {user} = useSelector((store) => store.persistedReducer.user)
+
+  return (
+    <Suspense fallback={'...Loading'}>
+            {
+                !user.login.length ?
+                    <AuthRouting/>
+                    :
+                   <PrivateRouting/>
+            }
+    </Suspense>
+  );
+}
+
+export default App;
+
